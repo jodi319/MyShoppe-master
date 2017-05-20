bebef2d6b8c746b3d2bf24544d6971ac32d1c814
jodi319 <jodi319@gmail.com>
2017-05-20 23:37:59 +0200

    Add project files.

---
+++ MyShop/App.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MyShop
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new NavigationPage(new HomePage())
+			{
+				BarTextColor = Color.White,
+                BarBackgroundColor = Color.FromHex("#2B84D3")
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ MyShop/Services/OfflineDataStore.cs
@@ -0,0 +1,79 @@
+﻿using Plugin.Messaging;
+using MyShop.Services;
+using Newtonsoft.Json;
+using PCLStorage;
+using Plugin.EmbeddedResource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+//[assembly: Dependency(typeof(OfflineDataStore))]
+namespace MyShop.Services
+{
+    public class OfflineDataStore : IDataStore
+    {
+
+        public async Task<IEnumerable<Store>> GetStoresAsync()
+        {
+            var rootFolder = FileSystem.Current.LocalStorage;
+            var json = ResourceLoader.GetEmbeddedResourceString(Assembly.Load(new AssemblyName("MyShop")), "stores.json");
+            return await Task.Run(() => JsonConvert.DeserializeObject<List<Store>>(json));
+        }
+
+        public async Task<Feedback> AddFeedbackAsync(Feedback feedback)
+        {
+            var emailTask = MessagingPlugin.EmailMessenger;
+            if (emailTask.CanSendEmail)
+            {
+                emailTask.SendEmail("james.montemagno@xamarin.com", "My Shop Feedback", feedback.ToString());
+            }
+
+            return await Task.Run(() => { return feedback; });
+        }
+
+        public async Task<Store> AddStoreAsync(Store store)
+        {
+            return await Task.Run(() => { return store; });
+        }
+
+        public async Task<IEnumerable<Feedback>> GetFeedbackAsync()
+        {
+            return await Task.Run(() => { return new List<Feedback>(); });
+        }
+
+
+        public Task Init()
+        {
+            return Task.Run(() => { });
+        }
+
+        public async Task<bool> RemoveFeedbackAsync(Feedback feedback)
+        {
+            return await Task.Run(() => { return true; });
+        }
+
+        public async Task<bool> RemoveStoreAsync(Store store)
+        {
+            return await Task.Run(() => { return true; });
+        }
+
+        public Task SyncFeedbacksAsync()
+        {
+            return Task.Run(() => { });
+        }
+
+        public Task SyncStoresAsync()
+        {
+            return Task.Run(() => { });
+        }
+
+        public async Task<Store> UpdateStoreAsync(Store store)
+        {
+            return await Task.Run(() => { return store; });
+        }
+    }
+}
